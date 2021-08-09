19613cdc69d2941232dc8b75177bad3c296e1342
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2021-08-08 17:11:17 -0700

    Manual implementation of TT

--- keyboards/planck/keymaps/amadeus/keymap.c
+++ keyboards/planck/keymaps/amadeus/keymap.c
@@ -17,6 +17,15 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
+uint16_t tt_timeout      = 300;
+uint16_t tt_lock_pressed = 3;
+uint16_t press_count     = 0;
+uint16_t tt_timer        = 0;
+
+enum custom_keycodes {
+  TT_LL = SAFE_RANGE,
+};
+
 enum planck_layers {
   _BASE,
   _LOWER,
@@ -41,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,    KC_Y,  KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_RCTL,  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,    KC_H,  KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,    KC_N,  KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-    KC_ESC,   KC_LCTL,  KC_LALT,  KC_LGUI,  TT(1),  KC_SPC,         MO(2),   KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT
+    KC_ESC,   KC_LCTL,  KC_LALT,  KC_LGUI,  TT_LL,  KC_SPC,         MO(2),   KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT
   ),
 
 /* Lower
@@ -244,4 +253,33 @@ layer_state_t layer_state_set_user(layer_state_t state) {
       break;
   }
   return state;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (timer_elapsed(tt_timer) > tt_timeout) {
+    press_count = 0;
+  }
+  switch (keycode) {
+    // This is my custom version of the TT function, that doesn't introduce lag
+    // in the layer response
+    case TT_LL: {
+      if (record->event.pressed) {
+        if (layer_state_is(_LOWER)) {
+          layer_off(_LOWER);
+          press_count = 0;
+          tt_timer = 0;
+        } else {
+          press_count += 1;
+          layer_on(_LOWER);
+          tt_timer = timer_read();
+        }
+      } else {
+        if (press_count < tt_lock_pressed) {
+          layer_off(_LOWER);
+        }
+      }
+      break;
+    }
+  }
+  return true;
 }
