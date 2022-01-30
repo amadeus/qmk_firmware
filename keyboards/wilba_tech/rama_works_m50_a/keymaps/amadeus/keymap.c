c4bb80b59bf1cd9e9d39f2b44032d1850ac951d5
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2022-01-29 16:49:54 -0800

    RAMA Configuration
    * Proper implementation of sidebar RGB
        * Layer based indicators
        * Additional shift logic
        * CapsLock logic
        * Trying out a 10 minute timeout
    * Disable default RGB animations from ever running (I hope)

--- keyboards/wilba_tech/rama_works_m50_a/keymaps/amadeus/keymap.c
+++ keyboards/wilba_tech/rama_works_m50_a/keymaps/amadeus/keymap.c
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------------------------------------------------------------'
  */
   [_BASE] = LAYOUT(
-    H1_INC,    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,    KC_Y,  KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
-    H1_DEC,    KC_RCTL,  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,    KC_H,  KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    H2_INC,    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,    KC_N,  KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-    H2_DEC,    KC_ESC,   KC_LCTL,  KC_LALT,  KC_LGUI,  TT_LL,  KC_SPC,         MO(2),   KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT
+    TO(3),  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,    KC_Y,  KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
+    TO(2),  KC_RCTL,  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,    KC_H,  KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    TO(0),  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,    KC_N,  KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+    TO(1),  KC_ESC,   KC_LCTL,  KC_LALT,  KC_LGUI,  TT_LL,  KC_SPC,         MO(2),   KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT
   ),
 
 
@@ -147,3 +147,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     }
     return true;
 }
+
+// NOTE(amadeus): Enable this when debugging
+// void keyboard_post_init_user(void) { debug_enable = true; }
